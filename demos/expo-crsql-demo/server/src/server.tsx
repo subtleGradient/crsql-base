e7636c8e88931519cce4ea3605bbfe787bb0ade6
Tom Aylott <oblivious@subtlegradient.com>
2025-08-09 17:36:26 -0400

    server

---
+++ demos/expo-crsql-demo/server/src/server.tsx
@@ -0,0 +1,44 @@
+import { serve } from "bun";
+import expoIndex from "../../dist/index.html";
+import index from "./index.html";
+
+const server = serve({
+	routes: {
+		// Serve index.html for all unmatched routes.
+		"/flarm": index,
+
+		"/*": expoIndex,
+
+		"/api/hello": {
+			async GET(req) {
+				return Response.json({
+					message: "Hello, world!",
+					method: "GET",
+				});
+			},
+			async PUT(req) {
+				return Response.json({
+					message: "Hello, world!",
+					method: "PUT",
+				});
+			},
+		},
+
+		"/api/hello/:name": async (req) => {
+			const name = req.params.name;
+			return Response.json({
+				message: `Hello, ${name}!`,
+			});
+		},
+	},
+
+	development: process.env.NODE_ENV !== "production" && {
+		// Enable browser hot reloading in development
+		hmr: true,
+
+		// Echo console logs from the browser to the server
+		console: true,
+	},
+});
+
+console.log(`🚀 Server running at ${server.url}`);
